c2e53f018a200f6b03c676fc6ba123d314a038de
chrdebru <christophe.debruyne@gmail.com>
2024-08-04 22:33:45 +0200

    Trying to disable feed

---
+++ _plugins/feed_generator_override.rb
@@ -0,0 +1,13 @@
+
+# frozen_string_literal: true
+
+module JekyllFeed
+  class Generator < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    # Main plugin action, called by Jekyll-core
+    def generate(_site)
+    end
+  end
+end
